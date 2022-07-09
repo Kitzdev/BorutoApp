3cdb2133b88a6d40da9bcddb582d729cb61eda7f
Kitbert <kitbert77@gmail.com>
2022-07-09 23:21:49 +0700

    Create DatabaseConverter class

--- app/src/main/java/dev/kitzdev/borutoapp/data/local/BorutoDatabase.kt
+++ app/src/main/java/dev/kitzdev/borutoapp/data/local/BorutoDatabase.kt
@@ -1,12 +1,14 @@
 package dev.kitzdev.borutoapp.data.local
 
 import androidx.room.Database
+import androidx.room.TypeConverters
 import dev.kitzdev.borutoapp.data.local.dao.HeroDao
 import dev.kitzdev.borutoapp.data.local.dao.HeroRemoteKeyDao
 import dev.kitzdev.borutoapp.domain.modal.Hero
 import dev.kitzdev.borutoapp.domain.modal.HeroRemoteKey
 
 @Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
+@TypeConverters(DatabaseConverter::class)
 abstract class BorutoDatabase {
     abstract fun heroDao(): HeroDao
     abstract fun heroRemoteKeyDao(): HeroRemoteKeyDao
