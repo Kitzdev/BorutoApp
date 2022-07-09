cc5753e3b7186b766b419d18cae67528bf670bb4
Kitbert <kitbert77@gmail.com>
2022-07-09 22:14:08 +0700

    Setup Navigation

--- app/src/main/java/dev/kitzdev/borutoapp/MainActivity.kt
+++ app/src/main/java/dev/kitzdev/borutoapp/MainActivity.kt
@@ -3,13 +3,21 @@ package dev.kitzdev.borutoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import dev.kitzdev.borutoapp.navigation.SetupNavGraph
 import dev.kitzdev.borutoapp.ui.theme.BorutoAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BorutoAppTheme {
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
             }
         }
     }
---
+++ app/src/main/java/dev/kitzdev/borutoapp/util/Constants.kt
@@ -0,0 +1,7 @@
+package dev.kitzdev.borutoapp.util
+
+object Constants {
+
+    const val DETAILS_ARGUMENT_KEY = "heroId"
+
+}
